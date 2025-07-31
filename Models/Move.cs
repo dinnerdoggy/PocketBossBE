f1814d62e478decb0129d96f8b300757210bb92c
dinnerdoggy <31261276+dinnerdoggy@users.noreply.github.com>
2025-07-31 13:51:16 -0500

    nullable cleanup

--- Models/Move.cs
+++ Models/Move.cs
@@ -20,13 +20,13 @@ public enum TargetType
 public class Move
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public MoveType MoveType { get; set; }
     public TargetType TargetType { get; set; }
     public int Power { get; set; }
 
     // Navigation Properties
-    public List<BossMove> BossMoves { get; set; }
-    public List<PCMove> PCMoves { get; set; }
+    public List<BossMove>? BossMoves { get; set; }
+    public List<PCMove>? PCMoves { get; set; }
 }
--- Models/PC.cs
+++ Models/PC.cs
@@ -3,22 +3,22 @@ namespace PocketBoss.Models;
 public class PC
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public int MaxHealth { get; set; }
     public int CurrentHealth { get; set; }
 
     // Navigation Propery for join table
-    public List<PCMove> PCMoves { get; set; }
+    public List<PCMove>? PCMoves { get; set; }
 }
 
 public class PCMove
 {
     public int PCId { get; set; }
-    public PC PC { get; set; }
+    public PC? PC { get; set; }
 
     public int MoveId { get; set; }
-    public Move Move { get; set; }
+    public Move? Move { get; set; }
 
     public int SlotNumber { get; set; }
 }
